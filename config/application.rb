3f236d73e9cca00d173a054b122b79d6b8cf9316
Eric London <eric.london@arcadiasolutions.com>
2017-01-26 08:48:30 -0500

    added logging gems and config

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module DockerRailsLogstash
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.lograge.enabled = true
+    config.lograge.formatter = Lograge::Formatters::Logstash.new
   end
 end
